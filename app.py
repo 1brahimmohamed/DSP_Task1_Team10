36592f848f429fdacea9208d6c137e24acc8141d
Ibrahim <imimazim7@gmail.com>
2022-10-19 23:56:56 +0200

    added add-signal api

--- app.py
+++ app.py
@@ -49,14 +49,14 @@ def generate_signal():
 
     freq = float(request.values['frequency'])  # get frequency from request & cast it to float
     amp = float(request.values['amplitude'])   # get amplitude from request & cast it to float
-    signal_type = request.values['type']       # get signal type (sine or cosine) from request
+    signal_amp_type = request.values['type']       # get signal type (sine or cosine) from request
 
     # generate time for signal generation
     time = np.arange(0, 5, 0.01)
 
-    if signal_type == 'sine':
+    if signal_amp_type == 'sine':
         signal_values = amp * np.sin(2 * pi * freq * time)  # generate the sine signal values
-    elif signal_type == 'cosine':
+    elif signal_amp_type == 'cosine':
         signal_values = amp * np.cos(2 * pi * freq * time)  # generate the cosine signal values
     else:
         return 'invalid signal type'  # invalid request
@@ -71,9 +71,15 @@ def generate_signal():
 def sample_signal():
 
     # get up parameters for signal sampling
-    time_values = [float(i) for i in request.form.getlist('time[]')]            # get time values from request & cast it to float
-    signal_amp_values = [float(x) for x in request.form.getlist('values[]')]    # get amplitude from request & cast it to float
-    sampling_frequency = int(request.values['samplingFrequency'])               # get signal type (sine or cosine) from request
+
+    # get time values from request & cast it to float
+    time_values = [float(i) for i in request.form.getlist('time[]')]
+
+    # get amplitude from request & cast it to float
+    signal_amp_values = [float(x) for x in request.form.getlist('values[]')]
+
+    # get signal type (sine or cosine) from request
+    sampling_frequency = int(request.values['samplingFrequency'])
 
     # sample the signal
     sampled_time_values, sampled_signal_values = sample(time_values, signal_amp_values, sampling_frequency)
@@ -82,6 +88,26 @@ def sample_signal():
     sampled_signal_result = list(sampled_time_values), list(sampled_signal_values)
 
     return list(sampled_signal_result)
+
+
+@app.route('/add-signals', methods=['POST'])
+def add_signal():
+
+    # get up parameters for signal adding #
+
+    # get amplitude of first signal from request & cast it to float array
+    signal_1_amp = [float(i) for i in request.form.getlist('signal1[]')]
+
+    # get amplitude of first signal from request & cast it to float array
+    signal_2_amp = [float(x) for x in request.form.getlist('signal2[]')]
+
+    signal_1_amp_np_array = np.array(signal_1_amp)      # convert python list to np array for easy adding amplitude
+    signal_2_amp_np_array = np.array(signal_2_amp)      # convert python list to np array for easy adding amplitude
+
+    # add the signals
+    added_signal = signal_1_amp_np_array + signal_2_amp_np_array
+
+    return list(added_signal)
 
 # --------------------------------------------------------------------------------------#
 #                                       Run App
