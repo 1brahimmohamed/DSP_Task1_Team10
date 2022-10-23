9b585407bb0165444bcb220fc13f717d69165045
Ibrahim <imimazim7@gmail.com>
2022-10-23 22:35:30 +0200

    fixed bugs

--- static/js/Generate Signal.js
+++ static/js/Generate Signal.js
@@ -8,37 +8,85 @@ class GenerateSignal {
         /** initial frequency = 0 **/
         this.amp = 0;
         /** initial amplitude = 0 **/
-        this.type = 'sine'      /** initial signal type -> sine wave **/
+        this.type = 'sine'
+        /** initial signal type -> sine wave **/
         this.samplingFrequency = 0;
 
-        this.xData = []
-        this.yData = []
+        /** Signal X,Y Values **/
 
-        /**
-         * Plotting Configurations
-         * **/
+        this.signalsCount = 0;
+        this.signalsList = {}
+
+        /** Signal Plotting Configurations **/
         this.data = [
             {
-                x: this.xData,
-                y: this.yData,
+                x: [0],
+                y: [0],
                 mode: "lines",
                 type: "scatter"
             }
         ];
 
+        /** Noise Plotting Configurations **/
+        this.noiseData = [{
+            x: [],
+            y: [],
+            mode: "lines",
+            type: "scatter"
+        }]
+
+        /**   Sampled Signal Data Configurations  */
+        this.sampledData = [{
+            x: [],
+            y: [],
+            mode: "lines",
+            type: "markers",
+            line: {
+                color: '#004072'
+            }
+        }]
+
+        /**   Reconstructed Signal Data Configurations  */
+        this.reconstructedData = [{
+            x: [],
+            y: [],
+            mode: "lines",
+            type: "scatter",
+            line: {
+                color: '#00b4fa'
+            }
+        }]
+
+        /**  Plot Configuration Object
+         *  responsive: -> make responsive layout
+         *  editable: -> enable editing options for the plot
+         *  displaylogo: -> hide plotly logo
+         * **/
         this.config = {
             responsive: true,
             editable: true,
             displaylogo: false
         };
 
+
+        /**   Plot layout settings  */
         this.layout = {
-            title: "Signal displayed here",
-            font: {size: 14},
+            title: "Main Plot",
+            xaxis: {
+                title: {
+                    text: 'time (s)',
+                }
+            },
+            yaxis: {
+                title: {
+                    text: 'amplitude (mv)'
+                }
+            },
+            font: {size: 12},
         };
     }
 
-    constructNewSignal(amplitude, frequency, type) {
+    constructNewSignal(amplitude, frequency, type = 'sine') {
 
         /**  empty string for expression to be expressed  **/
         this.freq = frequency;
@@ -72,19 +120,14 @@ class GenerateSignal {
             }
         });
 
-        this.xData = xData;
-        this.yData = yData;
-
-        this.data = [
+        return [
             {
-                x: this.xData,
-                y: this.yData,
+                x: xData,
+                y: yData,
                 mode: "lines",
                 type: "scatter"
             }
-        ]
-
-        return this.data;
+        ];
     }
 
     plot(amplitude, frequency, type, div) {
@@ -92,97 +135,27 @@ class GenerateSignal {
         /**   generate new signal  **/
         const data = this.constructNewSignal(amplitude, frequency, type);
 
-
         /**  plot this signal   **/
         Plotly.newPlot(div, data, this.layout, this.config);
     }
 
-    async changeAmplitude(amplitude) {
+    changeAmplitude(amplitude) {
+        /** set the amplitude of this signal to new amplitude **/
         this.amp = amplitude
 
         /**  generate new signal with the new amplitude   **/
-        let data = this.constructNewSignal(amplitude, this.freq, this.type);
-
-        /**  set layout   **/
-        let layout = {
-            title: "Signal displayed here",
-            font: {
-                size: 18
-            },
-            yaxis: {
-                range: [-10, 10]
-            }
-
-        };
-
-        /**   plot the new signal  **/
-        await Plotly.animate(
-            "plot1",
-            {
-                layout: layout,
-                data: data,
-                traces: [0]
-
-            },
-            {
-                transition: {
-                    duration: 500,
-                    easing: "cubic-in-out",
-                },
-                frame: {
-                    duration: 500,
-                },
-            },
-            this.config
-        );
-
+        this.constructNewSignal(amplitude, this.freq, this.type);
     }
 
-    async changeFrequency(frequency) {
+    changeFrequency(frequency) {
+        /** set the frequency of this signal to new frequency **/
         this.freq = frequency
 
         /**  generate new signal with the new frequency   **/
-        let data = this.constructNewSignal(this.amp, frequency, this.type);
-
-        /**  set layout   **/
-        let layout = {
-            title: "Sampled Signal",
-            font: {
-                size: 18
-            },
-            yaxis: {
-                range: [-11, 11]
-            },
-            xaxis: {
-                range: [0, 6]
-            }
-
-        };
-
-        /**   plot the new signal  **/
-        await Plotly.animate(
-            "plot1",
-            {
-                layout: layout,
-                data: data,
-                traces: [0]
-
-            },
-            {
-                transition: {
-                    duration: 500,
-                    easing: "cubic-in-out",
-                },
-                frame: {
-                    duration: 500,
-                },
-            },
-            this.config
-        );
-
+        this.constructNewSignal(this.amp, frequency, this.type);
     }
 
-    sampleSignal(rateOfSampling) {
+    sampleSignal(rateOfSampling, data = this.data) {
         let sampledX = [];
         let sampledY = [];
         this.samplingFrequency = rateOfSampling;
@@ -190,7 +163,7 @@ class GenerateSignal {
          *  ajax call to retrieve sampled data from the server
          *  method -> post
          *  url -> visit sampling api
-         *  dataType -> returned response formate
+         *  dataType -> returned response format
          *  data -> data to be sent to the server with the x, y values
          *  res[0] -> samples time array
          *  res[1] -> sampled values array
@@ -202,8 +175,8 @@ class GenerateSignal {
             dataType: 'json',
             async: false,
             data: {
-                time: this.xData,
-                values: this.yData,
+                time: data[0].x,
+                values: data[0].y,
                 samplingFrequency: this.samplingFrequency,
             },
             success: function (res, status, xhr) {
@@ -211,48 +184,138 @@ class GenerateSignal {
                 sampledY = res[1];
             }
         });
+        /**  set the sampling data to the data coming from the server   */
+        this.sampledData = [
+            {
+                x: sampledX,
+                y: sampledY,
+                type: "line",
+                mode: 'markers'
+            }
+        ]
+    }
 
-        let trace0 = {
-            type: "scatter",
-            mode: "lines",
-            name: 'Signal',
-            x: this.xData,
-            y: this.yData,
-            line: {color: '#2d7fb8'}
+    addNoise(SNR) {
+
+        if (SNR >= 0) {
+            let noiseData;
+            this.noiseData[0].x = this.data[0].x
+
+            /**
+             *  ajax call to retrieve noise data from the server
+             *  method -> post
+             *  url -> visit noise api
+             *  dataType -> returned response format
+             *  data -> data to be sent to the server with the signal time & amplitude
+             *  res -> noise signal array
+             * **/
+
+            $.ajax({
+                method: 'POST',
+                url: 'http://127.0.0.1:5002/add-noise',
+                async: false,
+                dataType: 'json',
+                data: {
+                    time: this.data[0].x,
+                    signal: this.data[0].y,
+                    SNR: 5
+                },
+                success: function (res, status, xhr) {
+                    noiseData = res;
+                }
+            });
+
+            /**  set the noise data to the data coming from the server   */
+            this.noiseData[0].x = this.data[0].x
+            this.noiseData[0].y = this.data[0].y
         }
+    }
 
+    plotNoise() {
+        /**  plot the noise on first canvas   **/
+        Plotly.newPlot("canvas-1", this.noiseData, this.layout, this.config);
+    }
 
-        let trace1 = {
-            type: "line",
-            mode: 'markers',
-            name: 'Sampled',
-            x: sampledX,
-            y: sampledY,
-            line: {color: '#ff01a2'}
-        }
+    reconstructSignal(rateOfSampling) {
+        let reconstructedX = []
+        let reconstructedY = []
 
-        let data = [trace0,trace1]
+        /** time value of the reconstructed Signal**/
+        reconstructedX = this.data[0].x;
 
-        /**  set layout   **/
-        // let layout = {
-        //     title: "Signal displayed here",
-        //     font: {
-        //         size: 12
-        //     },
-        //     yaxis: {
-        //         range: [-10, 10]
-        //     },
-        //     xaxis: {
-        //         range: [0, 6]
-        //     }
-        // }
+        /**
+         *  ajax call to retrieve reconstructed data from the server
+         *  method -> post
+         *  url -> visit reconstruction api
+         *  dataType -> returned response format
+         *  data -> data to be sent to the server with the sampled signal & sampling frequency
+         *  res -> reconstructed signal array
+         * **/
 
-        /**   plot the new signal  **/
-        Plotly.react(
-            "canvas-1",
+        $.ajax({
+            method: 'POST',
+            url: 'http://127.0.0.1:5002/reconstruct-signal',
+            async: false,
+            dataType: 'json',
+            data: {
+                sampledSignal: this.sampledData[0].y,
+                sampledSignalFrequency: this.samplingFrequency,
+            },
+            success: function (res, status, xhr) {
+                reconstructedY = res;
+            }
+        });
+
+        /**  set the reconstruction data to the data coming from the server   */
+        this.reconstructedData[0].x = reconstructedX;
+        this.reconstructedData[0].y = reconstructedY
+    }
+
+    UpdateCanvas2(rateOfSampling) {
+
+        /** reconstruct signal with the new sampling rate   **/
+        this.reconstructSignal(rateOfSampling)
+
+        /** set the plot name of the signals **/
+        this.reconstructedData[0].name = 'Reconstructed'
+        this.sampledData[0].name = 'Sampled'
+
+        Plotly.newPlot(
+            "canvas-2",
+            [
+                this.reconstructedData[0],
+                this.sampledData[0]
+            ],
+            {
+                title: "Processed Plot of the Signal",
+                font: {
+                    size: 12
+                },
+                xaxis: {
+                    title: {
+                        text: 'time(s)'
+                    }
+                },
+                yaxis: {
+                    title: {
+                        text: 'amplitude(mV)'
+                    }
+                }
+            },
+            this.config,
+        );
+
+    }
+
+    async motionPlot(canvas, data) {
+
+        /**   use Plotly .animate Function **/
+        await Plotly.animate(
+            canvas,
             {
                 layout: this.layout,
-                data: [trace0,trace1],
+                data: data,
+                traces: [0]
 
             },
             {
@@ -264,8 +327,99 @@ class GenerateSignal {
                     duration: 500,
                 },
             },
-            this.config
+            this.config,
+            this.layout
         );
+    }
 
+    addSignals(amplitude, frequency, type) {
+        let added = this.constructNewSignal(amplitude, frequency, type)
+        let additionResult = []
+
+        /**   first time make this signal = the signal data of y  **/
+        if (this.signalsCount === 0) {
+            this.data[0].y = added[0].y
+            this.data[0].x = added[0].x
+        } else {
+            $.ajax({
+                method: 'POST',
+                url: 'http://127.0.0.1:5002/add-signals',
+                async: false,
+                dataType: 'json',
+                data: {
+                    signal1: this.data[0].y,
+                    signal2: added[0].y,
+                },
+                success: function (res, status, xhr) {
+                    additionResult = res;
+                }
+            });
+            this.data[0].y = additionResult
+        }
+
+        this.signalsCount++;
+        this.signalsList[`Signal${this.signalsCount}`] = added;
+    }
+
+    deleteSignal(signalName) {
+
+        let deleted = this.signalsList[signalName]
+        let subtractionResult = []
+
+        $.ajax({
+            method: 'POST',
+            url: 'http://127.0.0.1:5002/subtract-signals',
+            async: false,
+            dataType: 'json',
+            data: {
+                signal1: deleted[0].y,
+                signal2: this.data[0].y,
+            },
+            success: function (res, status, xhr) {
+                subtractionResult = res;
+            }
+        });
+
+        this.data[0].y = subtractionResult;
+        delete this.signalsList[signalName];
+    }
+
+    openSignalFromPC(file) {
+        let time = []
+        let values = []
+        let keys = Object.keys(file[0])
+
+        /** map through the file to extract values of the file **/
+        file.map((d) => {
+            time.push(d[keys[0]])
+            values.push(d[keys[1]])
+        })
+
+        ++this.signalsCount;             /** increment the added signals array **/
+
+        /**  set the value of the current signal to the values coming from the file  **/
+        this.data[0].x = time;
+        this.data[0].y = values;
+
+        /**  add the signal data to the list of the signals   **/
+        this.signalsList[`Signals ${this.signalsCount}`] = [
+            {
+                x: this.data[0].x,
+                y: this.data[0].y,
+                mode: 'lines',
+                type: 'scatter'
+            }
+        ]
+    }
+
+    exportSignalToCSV(x, y) {
+
+        let csv = []        // create object 'csv' to store signal values
+        for (let i = 0; i < x.length; ++i) {
+            csv.push([x[i], y[i]])
+        }
+        /**  object returned to be converted to CSV   **/
+        return csv
     }
 }
+
--- static/js/Page Driver.js
+++ static/js/Page Driver.js
@@ -40,75 +40,222 @@ reconstructionTab.onclick = function () {
 }
 
 /**************************************************************************************
- *                                  Signal Generation
+ *                                  Variables Definition
  **************************************************************************************/
 
-let generateBtn = document.getElementById('generate'),
-    signals = [],
-    selectedUp,
-    i = 0;
+let signal = new GenerateSignal()
 
-generateBtn.onclick = function () {
-    let signal,
-        freq, amp, type
-    if (i%2 === 0){
-        signal = new GenerateSignal();
-        amp = 4
-        freq = 2
-        type = 'sine'
+/**      Buttons      **/
+let generateBtn = document.getElementById('add'),
+    uploadBtn = document.getElementById('selectedFile'),
+    saveBtn = document.getElementById('save'),
+    removeSignalBtn = document.getElementById('remove-signal'),
+    redirect = document.getElementById('download-redirect')
+
+/**     Combo Boxes  **/
+let signalsComboBox = document.getElementById('current-components')
+
+
+/**      Input Fields       **/
+let frequencyInputField = document.getElementById('frequency'),
+    amplitudeInputField = document.getElementById('amplitude'),
+    typeInputField = document.getElementById('type'),
+    samplingRateInputField = document.getElementById('sample'),
+    noiseInputField = document.getElementById('noise')
+
+/**    tables     **/
+let signalsTable = document.getElementById('signals')
+
+
+
+/**************************************************************************************
+ *                             Event Handlers (On Button Click)
+ **************************************************************************************/
+
+// Generate Signal Button
+generateBtn.onclick = async function () {
+
+    /**   if the user hasn't written any input >> show invalid modal card  **/
+    if (frequencyInputField.value === '' || amplitudeInputField.value === '') {
+        Swal.fire({
+            title: 'Invalid Input',
+            text: "Please fill the signal data",
+            icon: 'error',
+            confirmButtonColor: '#004072',
+        })
+    }
+    /**   if user write valid input >> procced  **/
+    else {
+        let type = typeInputField.value
+        let sampFreq = samplingRateInputField.value
+
+        // default value for sine Type
+        if (type === '' || type !== 'sine' || type !== 'cosine')
+            type = 'sine'
+
+        // default value for sampling frequency
+        if (sampFreq === '')
+            sampFreq = 2 * frequencyInputField.value
+
+
+        // generate signal
+        signal.addSignals(amplitudeInputField.value, frequencyInputField.value, type, sampFreq);
+        signal.samplingFrequency = sampFreq
+
+        let signalCount = signal.signalsCount
+        let item = document.createElement('tr');
+        item.setAttribute('id', `signal-table${signalCount}`);
+        item.innerHTML = `
+                    <td>Signal ${signalCount}</td>
+                    <td>${frequencyInputField.value} Hz</td>
+                    <td>${amplitudeInputField.value}</td>
+                    <td>${type}</td>
+    `
+        signalsTable.appendChild(item)
+
+        if (noiseInputField.value !== '') {
+            signal.addNoise(noiseInputField.value);
+            signal.motionPlot("canvas-1", signal.noiseData);
+            await signal.motionPlot("canvas-1", signal.noiseData);
+            signal.sampleSignal(samplingRateInputField.value, signal.noiseData);
+        } else {
+            signal.motionPlot("canvas-1", signal.data);         // plot signal
+            await signal.motionPlot("canvas-1", signal.data);   // animate plot
+            signal.sampleSignal(sampFreq);                             // sample the signal
+        }
+
+        signal.UpdateCanvas2(sampFreq);             // update down plot
+
+        let option = document.createElement("option");
+        option.text = ` (Signal${signalCount}) \t Amplitude= ${amplitudeInputField.value}, Frequency= ${frequencyInputField.value}`
+        option.value = `Signal${signalCount}`
+        signalsComboBox.appendChild(option);
+
+
+    }
+
+};
+
+saveBtn.onclick = ()=>{
+    let myCSVObject = []
+    if (noiseInputField.checked) {
+        myCSVObject = signal.exportSignalToCSV(signal.noiseData[0].x, signal.noiseData[0].y)
     }
     else{
-        signal = new GenerateSignal();
-        amp = 5
-        freq = 5
-        type = 'cosine'
+        myCSVObject = signal.exportSignalToCSV(signal.reconstructedData[0].x, signal.reconstructedData[0].y)
     }
-    i++
-    signal.plot(amp, freq, type, 'canvas-1');
+    let csv = 'x,y\n';
 
-    let item = document.createElement('tr');
-    item.innerHTML = `
-                    <td>Signal ${signals.length}</td>
-                    <td>${freq} Hz</td>
-                    <td>${amp}</td>
-                    <td>${type}</td>
-
-    `
-    document.getElementById('signals').appendChild(item)
-    signals.push(signal)
-    rowSelector()
-}
-
-
-// Plotly.newPlot('canvas-2',
-//     [
-//         {
-//             x: [0],
-//             y: [0]
-//         }
-//         ],
-//     {
-//         yaxis: {
-//             range: [-11, 11]
-//         }
-//     })
-
-let samplingInput = document.getElementById('sample');
-samplingInput.onclick = function () {
-    let samplingRate = samplingInput.value
-    if (samplingRate !== 0)
-        signals[0].sampleSignal(samplingRate)
-}
-
-
-function rowSelector(){
-    $(".signal-table tr").click(function () {
-        $(this).addClass('selected').siblings().removeClass('selected');
-        selectedUp = $(this).index() - 1
-
-        let barValues = $(".form-inline div").children()
-        barValues[2].value = signals[selectedUp].samplingFrequency
-        barValues[1].value = signals[selectedUp].amp
-        barValues[0].value = signals[selectedUp].freq
+    myCSVObject.forEach(function(row) {
+        csv += row.join(',');
+        csv += "\n";
     });
+    redirect.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+    redirect.download = `Signal${signal.signalsCount}`;
 }
+
+removeSignalBtn.onclick = async () => {
+    console.log('gowa')
+    signal.deleteSignal(signalsComboBox.value);
+
+    // let removed = document.getElementById(`signal-table${signalsComboBox.selectedIndex}`)
+    // console.log(signalsComboBox.selectedIndex)
+    // removed.remove()
+
+    if (noiseInputField.value > 0) {
+        signal.addNoise(noiseInputField.value);
+        signal.motionPlot("canvas-1", signal.noiseData);
+        await signal.motionPlot("canvas-1", signal.noiseData);
+        signal.sampleSignal(samplingRateInputField.value, signal.noiseData);
+    } else {
+        signal.motionPlot("canvas-1", signal.data);
+        await signal.motionPlot("canvas-1", signal.data);
+        signal.sampleSignal(samplingRateInputField.value);
+
+    }
+    signal.UpdateCanvas2(samplingRateInputField.value);
+    signalsComboBox.remove(signalsComboBox.selectedIndex)
+}
+
+/**************************************************************************************
+ *                             Event Handlers (On Input Change)
+ **************************************************************************************/
+
+uploadBtn.oninput = function (event) {
+    let sampFreq = samplingRateInputField.value
+    if (sampFreq === '')
+        sampFreq = 0
+    let file = event.target.files[0],
+        fileReader = new FileReader();
+
+    fileReader.readAsText(file)
+    fileReader.onload = async function (ev) {
+        let csv = ev.target.result,
+            parsedFile = d3.csvParse(csv)
+        signal.openSignalFromPC(parsedFile)
+
+        let option = document.createElement("option");
+        option.text = `(Signal${signal.signalsCount})`
+        option.value = `Signal${signal.signalsCount}`
+        signalsComboBox.appendChild(option)
+        signal.motionPlot('canvas-1', signal.data)
+        await signal.motionPlot('canvas-1', signal.data)
+        signal.sampleSignal(sampFreq)
+        signal.UpdateCanvas2(sampFreq)
+
+        let item = document.createElement('tr');
+        item.setAttribute('id', `signal-table${signal.signalsCount}`);
+        item.innerHTML = `
+                    <td>Signal ${signal.signalsCount}</td>
+                    <td>XX Hz</td>
+                    <td>XX </td>
+                    <td>Imported</td>
+    `
+        signalsTable.appendChild(item)
+    }
+}
+
+
+samplingRateInputField.onchange =  async function () {
+    let samplingRate = samplingRateInputField.value;
+    if(noiseInputField.value > 1){
+        //recalculate the signal
+        signal.sampleSignal(samplingRate, signal.noiseData);
+    } else {
+        signal.sampleSignal(samplingRate);
+    }
+    signal.UpdateCanvas2(samplingRate);
+};
+
+
+/**************************************************************************************
+ *                       Application Logic (App Starts Here)
+ **************************************************************************************/
+
+Plotly.newPlot(
+    "canvas-1",
+    [
+        {
+            x: [0],
+            y: [0]
+        }
+    ],
+    {
+        title: "Main Plot",
+        font: {
+            size: 12
+        }
+    },
+    signal.config);
+
+Plotly.newPlot(
+    "canvas-2",
+    [{x: [0], y: [0]}],
+    {
+        title: "Processed Plot",
+        font: {
+            size: 12
+        }
+    },
+    signal.config
+);
